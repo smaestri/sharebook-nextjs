2a41386ced9a49fd93a7ca8db4f1baf024d1a8e6
Sylvain Maestri <sylvainmaestri@gmail.com>
2023-12-12 21:15:20 +0100

    test error message

--- app/books/new/page.tsx
+++ app/books/new/page.tsx
@@ -1,31 +1,17 @@
-import { db } from "@/app/lib/db";
-import { redirect } from "next/navigation";
+"use client"
+import { createBook } from "@/app/lib/actions";
+import { useFormState } from "react-dom";
 
 export default function Home() {
 
-  async function createBook(formData: FormData) {
-    'use server';
-
-    const title = formData.get('title') as string
-    const author = formData.get('author') as string
-
-    const book = await db.book.create({
-      data: {
-        title,
-        author
-      }
-    })
-    console.log(book)
-
-    redirect('/books')
-  }
-
+  const [formState, action] = useFormState(createBook,  {message: ''})
 
   return (<>
     <div>Create a book</div>
-    <form action={createBook}>
+    <form action={action}>
       Title: <input className="rounded-md border" name="title" type="text" />
       Author: <input className="rounded-md border" name="author" type="text" />
+      <div>{formState.message}</div>
       <button type="submit">Valid</button>
     </form>
   </>
--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -3,6 +3,25 @@
 import { redirect } from "next/navigation";
 import { db } from "./db";
 
+export async function createBook(formState : {message: string}, formData: FormData) {
+
+    return {
+        message: 'title longer'
+    }
+//     const title = formData.get('title') as string
+//     const author = formData.get('author') as string
+
+//     const book = await db.book.create({
+//       data: {
+//         title,
+//         author
+//       }
+//     })
+//     console.log(book)
+
+//     redirect('/books')
+  }
+
 export async function updateBook(id: number, formData: FormData) {
 
     console.log('updateBook ' + id + ' ' + formData)
